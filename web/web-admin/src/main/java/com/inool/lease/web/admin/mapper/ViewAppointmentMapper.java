ee14831024607cf028adcaf418f3a876c6f86a66
1489259653 <1489259653eric@gmail.com>
2024-11-29 23:02:50 +0800

    编写了预约控制层的分页查询预约信息方法。

--- web/web-admin/src/main/java/com/inool/lease/web/admin/mapper/ViewAppointmentMapper.java
+++ web/web-admin/src/main/java/com/inool/lease/web/admin/mapper/ViewAppointmentMapper.java
@@ -5,6 +5,7 @@ import com.inool.lease.web.admin.vo.appointment.AppointmentQueryVo;
 import com.inool.lease.web.admin.vo.appointment.AppointmentVo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
 * @author liubo
@@ -12,8 +13,10 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 * @createDate 2023-07-24 15:48:00
 * @Entity com.inool.lease.model.ViewAppointment
 */
+@Mapper
 public interface ViewAppointmentMapper extends BaseMapper<ViewAppointment> {
 
+    IPage<AppointmentVo> pageAppointmentByQuery(IPage<AppointmentVo> page, AppointmentQueryVo queryVo);
 }
 
 
