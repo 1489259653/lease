d5c8d95c69f3c9bb6115be806049524393624567
1489259653 <1489259653eric@gmail.com>
2024-11-30 16:07:43 +0800

    编写了房间控制层保存 更新，获取，删除等接口。修改了mybatis方言，idea 方言。

--- common/src/main/java/com/inool/lease/common/mybatisplus/MybatisPlusConfiguration.java
+++ common/src/main/java/com/inool/lease/common/mybatisplus/MybatisPlusConfiguration.java
@@ -13,7 +13,7 @@ public class MybatisPlusConfiguration {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MARIADB));
         return interceptor;
     }
 }
