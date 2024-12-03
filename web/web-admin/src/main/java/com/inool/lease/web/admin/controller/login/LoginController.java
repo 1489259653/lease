c450baf8fb64d667aab8571cc66d151d7ee7d8aa
1489259653 <1489259653eric@gmail.com>
2024-12-03 21:19:22 +0800

    编写了用户登录获取验证码控制层，以及增加了Redis配置信息，增加了常量类。

--- web/web-admin/src/main/java/com/inool/lease/web/admin/controller/login/LoginController.java
+++ web/web-admin/src/main/java/com/inool/lease/web/admin/controller/login/LoginController.java
@@ -2,22 +2,26 @@ package com.inool.lease.web.admin.controller.login;
 
 
 import com.inool.lease.common.result.Result;
+import com.inool.lease.web.admin.service.LoginService;
 import com.inool.lease.web.admin.vo.login.CaptchaVo;
 import com.inool.lease.web.admin.vo.login.LoginVo;
 import com.inool.lease.web.admin.vo.system.user.SystemUserInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "后台管理系统登录管理")
 @RestController
 @RequestMapping("/admin")
 public class LoginController {
-
+    @Autowired
+    private LoginService service;
     @Operation(summary = "获取图形验证码")
     @GetMapping("login/captcha")
     public Result<CaptchaVo> getCaptcha() {
-        return Result.ok();
+        CaptchaVo captchaVo = service.getCaptcha();
+        return Result.ok(captchaVo);
     }
 
     @Operation(summary = "登录")
